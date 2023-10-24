8a57021d0c5f79ee3dc9d39f28819d8d273b752c
Relampag0o <josemariar.l17@gmail.com>
2023-10-25 01:06:55 +0200

    finished

---
+++ Addition/src/SubtractionExe.java
@@ -0,0 +1,8 @@
+public class SubtractionExe {
+
+    public static void main(String[] args) {
+        int n1 = Integer.parseInt(args[0]);
+        int n2 = Integer.parseInt(args[1]);
+        System.out.println(n1 - n2);
+    }
+}
--- JavaCorrector/src/Launcher.java
+++ JavaCorrector/src/Launcher.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -10,11 +7,14 @@ import java.util.Random;
 public class Launcher {
     public LinkedList<Entry> entries;
     public LinkedList<Integer> outputs;
+    public LinkedList<File> files;
 
     public Launcher() {
         this.entries = new LinkedList<Entry>();
         this.outputs = new LinkedList<Integer>();
+        this.files = new LinkedList<File>();
         generateEntries();
+        chargeFiles();
     }
 
 
@@ -23,6 +23,19 @@ public class Launcher {
         for (int i = 0; i < 5; i++) {
             entries.add(new Entry(r.nextInt(1, 100), r.nextInt(1, 100)));
             outputs.add(entries.get(i).getResult());
+        }
+    }
+
+    public void chargeFiles() {
+        File f = new File("C:\\Users\\josem\\IdeaProjects\\PSP\\JavaCorrector\\samples");
+        File[] directories = f.listFiles();
+        assert directories != null;
+        // I got no clue what this lane does but seems to be checking if dirs !=null and throws an exception.
+        // the ide suggested it.
+        for (File fi : directories) {
+            if (fi.getName().contains("class")) {
+                files.add(fi);
+            }
         }
     }
 
@@ -36,32 +49,36 @@ public class Launcher {
     }
 
     public void launch() throws IOException {
+        // TODO: ASK IF THERE IS ANY WAY TO EXECUTE FEW PROCESSES AT ONCE
+        // ONE FOR EACH FILE SO THE READING GOES FASTER.
 
-        for (int i = 0; i < entries.size(); i++) {
-            ProcessBuilder pb = new ProcessBuilder("java", "AdditionExe", entries.get(i).getNum1() + "", entries.get(i).getNum2() + "");
-            Process p = pb.start();
-            BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String line;
-            while ((line = bfr.readLine()) != null) {
-                int result = Integer.parseInt(line);
-                if (outputs.get(i) == result) {
-                    System.out.println("Test passed." + " Result expected: " +
-                            outputs.get(i) + " Provided: " + result);
+        for (File f : files) {
+            System.out.println("Executing " + f.getName());
+            for (int i = 0; i < entries.size(); i++) {
+                ProcessBuilder pb = new ProcessBuilder("java", f.getName().split("\\.")[0], entries.get(i).getNum1() + "", entries.get(i).getNum2() + "");
+                pb.directory(new File("C:\\Users\\josem\\IdeaProjects\\PSP\\JavaCorrector\\samples"));
+                Process p = pb.start();
+                BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                String line;
+                while ((line = bfr.readLine()) != null) {
+                    int result = Integer.parseInt(line);
+                    if (outputs.get(i) == result) {
+                        System.out.println("Test passed." + " Result expected: " +
+                                outputs.get(i) + " Provided: " + result);
 
-                } else
-                    System.out.println("Test not passed." + " Result expected: " +
-                            outputs.get(i) + " Provided: " + result);
+                    } else
+                        System.out.println("Test not passed." + " Result expected: " +
+                                outputs.get(i) + " Provided: " + result);
+                }
+
+
             }
-
-
         }
-
     }
 
 
     public static void main(String[] args) throws IOException {
         Launcher l = new Launcher();
-        l.show();
         l.launch();
 
 
