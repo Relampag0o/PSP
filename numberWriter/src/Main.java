51b0fe446c3b99c187535d0694ee5f871af0a6f9
Relampag0o <josemariar.l17@gmail.com>
2023-11-05 13:36:28 +0100

    .

--- numberWriter/src/Main.java
+++ numberWriter/src/Main.java
@@ -1,5 +1,43 @@
+import java.util.LinkedList;
+import java.util.Random;
+
 public class Main {
+    public LinkedList<Process> processes;
+
+
+    public Main() {
+
+        this.processes = new LinkedList<Process>();
+    }
+
+    public int generateRandom() {
+        Random r = new Random();
+        return r.nextInt();
+
+    }
+
+
+    public void launch() {
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                ProcessBuilder pb = new ProcessBuilder("java", "Writer", i + "");
+                pb.start();
+            }
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main m = new Main();
+        m.launch();
+
+
     }
 }
