86a8a39c3aff6c156b5f244a15405b82d3c4d3f3
Relampag0o <josemariar.l17@gmail.com>
2023-12-06 15:38:07 +0100

    .
    .

---
+++ CustomSemaphore/src/Semaphore.java
@@ -0,0 +1,20 @@
+public class Semaphore {
+    private int maxThreads;
+
+    public Semaphore(int threads) {
+        this.maxThreads = threads;
+
+    }
+
+    public void acquire() {
+
+        while (true) {
+
+        }
+
+
+    }
+
+    public void release() {
+    }
+}
