0fca26dfab6bf3977cd5fa4c6cada0450323c5b5
Relampag0o <josemariar.l17@gmail.com>
2024-02-21 16:50:27 +0100

    .
    .

--- TestNET/src/Main.java
+++ TestNET/src/Main.java
@@ -1,18 +1,24 @@
 import java.io.*;
+import java.net.URI;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+
         try {
-            BufferedReader bfr = new BufferedReader(new FileReader(args[0]));
-            String line;
-
-            while ((line = bfr.readLine()) != null) {
-                new Thread(new Reader(line)).start();
-            }
-
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("http://localhost:3000/example/1"))
+                    .GET()
+                    .build();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());
+            System.out.println(response.body());
         } catch (Exception e) {
             e.printStackTrace();
         }
